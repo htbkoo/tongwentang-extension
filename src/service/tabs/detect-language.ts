973f7d7464bb8e00589c98e04500a80f03487f57
HeySP6 <h1017760@connect.hku.hk>
2022-10-02 16:55:34 +0200

    fix: Fix errors at content script on Android (`tabs.detectLanguage` is undefined)

--- src/service/tabs/detect-language.ts
+++ src/service/tabs/detect-language.ts
@@ -5,10 +5,17 @@ import { chsTypes, chtTypes, ZhType } from './tabs.constant';
 const langToZhtype = (lang: string): ZhType =>
   chsTypes.find(tag => tag === lang) ? ZhType.hans : chtTypes.find(tag => tag === lang) ? ZhType.hant : ZhType.und;
 
-export const detectLanguage = (tabId?: number): Promise<ZhType> =>
-  tabs
-    .detectLanguage(tabId)
-    .then(toLower)
-    .then(langToZhtype)
-    // INFO: some browsers may fail with detect language api
-    .catch(() => ZhType.und);
+export const detectLanguage = (tabId?: number): Promise<ZhType> => {
+  try {
+    return (
+      tabs
+        .detectLanguage(tabId)
+        .then(toLower)
+        .then(langToZhtype)
+        // INFO: some browsers may fail with detect language api
+        .catch(() => ZhType.und)
+    );
+  } catch (e) {
+    return Promise.resolve(ZhType.und);
+  }
+};
